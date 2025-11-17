a3fbbc8e3b4ece48d373332790ffef20777c1f3d
Rashid Shaikh <110743725+rashiddshaikh@users.noreply.github.com>
2025-11-17 19:40:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,18 +33,23 @@ export default function Page() {
       </RevealWrapper>
 
       <Suspense fallback={<></>}>
-        <Section
-          className=' mt-5'
-          contentClassName=' px-0 mx-0 md:px-0 mx-0 md:mx-0'
-          elevated={false}
-        >
-          <div className=' relative mt-5 block w-full py-1  md:py-2 '>
-            <h2 className='mb-14 text-center text-sm font-medium text-white md:text-lg  '>
-              Technologies I have used
-            </h2>
-            <SkillsSwiper  className="pt-4"/>
-          </div>
-        </Section>
+   <Section
+  className="mt-5"
+  contentClassName="px-0 mx-0 md:px-0 md:mx-0"
+>
+  <div className="relative mt-5 block w-full py-1 md:py-2">
+
+    <h2 className="text-center text-sm font-medium text-white md:text-lg">
+      Technologies I have used
+    </h2>
+
+    <div className="mt-20">
+      <SkillsSwiper />
+    </div>
+
+  </div>
+</Section>
+
       </Suspense>
 
       <ExperienceCard />
